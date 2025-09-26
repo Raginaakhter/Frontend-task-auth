00bae4c685d0df916f6f5dd04e4420fd3bedf053
Raginaakhter <raginaakhter052@gmail.com>
2025-09-26 14:35:49 +0600

    update

--- src/Authentation/RegisterForm.jsx
+++ src/Authentation/RegisterForm.jsx
@@ -63,10 +63,10 @@ const RegisterForm = () => {
     }
   };
 
-  const handleGoogleSignIn = () => {
-    console.log('Continuing with Google...');
-    console.log("Continuing with Google...");
-  };
+  // const handleGoogleSignIn = () => {
+  //   console.log('Continuing with Google...');
+  //   console.log("Continuing with Google...");
+  // };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 ">
